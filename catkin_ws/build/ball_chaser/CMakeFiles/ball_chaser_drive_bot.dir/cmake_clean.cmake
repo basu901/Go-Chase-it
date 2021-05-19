7af8735c1a911f636168c98e9bca784988aeb437
basu901 <basu901@gmail.com>
2021-05-19 17:04:48 +0000

    Added the srv setup

---
+++ catkin_ws/build/ball_chaser/CMakeFiles/ball_chaser_drive_bot.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ball_chaser_drive_bot.dir/src/drive_bot.cpp.o"
+  "/home/workspace/catkin_ws/devel/lib/ball_chaser/drive_bot.pdb"
+  "/home/workspace/catkin_ws/devel/lib/ball_chaser/drive_bot"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ball_chaser_drive_bot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
