3bbbca093fb24b12dbd6eab6a7517f205940658d
basu901 <basu901@gmail.com>
2022-05-25 07:54:40 +0000

    Updated files for contact sensor

--- catkin_ws/src/ball_chaser/src/drive_bot.cpp
+++ catkin_ws/src/ball_chaser/src/drive_bot.cpp
@@ -1,10 +1,21 @@
 #include "ros/ros.h"
 #include "geometry_msgs/Twist.h"
+#include "gazebo_msgs/ContactsState.h"
+#include "sensor_msgs/LaserScan.h"
 //TODO: Include the ball_chaser "DriveToTarget" header file
 #include "ball_chaser/DriveToTarget.h"
+
+#include<algorithm>
 #include<string>
+#include<limits>
+
 //ros::Publisher motor commands;
 ros::Publisher motor_command_publisher;
+ros::Subscriber bumper_information_subscriber;
+ros::Subscriber laser_scan_sub;
+
+int bumper_contact = 0;
+float min_dist = std::numeric_limits<float>::max() ;
 
 // TODO: Create a handle_drive_request callback function that executes whenever a drive_bot service is requested.
 // This function should publish the requested linear x and angluar velocities to the robot wheel joints
@@ -13,21 +24,40 @@ ros::Publisher motor_command_publisher;
 bool relay_robot_velocities(ball_chaser::DriveToTarget::Request& req,ball_chaser::DriveToTarget::Response& res)
 {
 	ROS_INFO("DriveToTarget received - x:%1.2f, z:%1.2f", (float)req.linear_x,(float)req.angular_z);
-	
+
 	float linear_x = (float)req.linear_x;
 	float angular_z = (float)req.angular_z;	
 
 	//Create a motor_command object of type geometry_msgs::Twist
 	geometry_msgs::Twist motor_command;
 	//Set wheel velocities, forward [0.5, 0.0]
+
+	if(bumper_contact && min_dist<0.2)
+		ROS_INFO("Move the ball, the robot has collided with it (hopefully)! Distance between robot and ball is %1.2f", min_dist);
+	
+	
 	motor_command.linear.x = linear_x;
 	motor_command.angular.z = angular_z;
+	
 	//Publish angles to drive the robot
 	motor_command_publisher.publish(motor_command);
 
 
 	res.msg_feedback = "Linear velocity is "+ std::to_string(linear_x) + ", Angular velocity is " + std::to_string(angular_z);
 	
+}
+
+
+void stop_robot(gazebo_msgs::ContactsState contact)
+{
+	if(contact.states.size()>0)
+		bumper_contact = 1;
+}
+
+void assign_scan(sensor_msgs::LaserScan laser)
+{
+	min_dist = *(std::min_element(std::begin(laser.ranges), std::end(laser.ranges)));
+
 }
 
 int main(int argc, char** argv)
@@ -44,6 +74,8 @@ int main(int argc, char** argv)
 	//Inform ROS master that we will be publishing a message of type geometry_msgs::Twist on the robot actuation topic
 	//with a publishing queue size of 10
 	motor_command_publisher = n.advertise<geometry_msgs::Twist>("/cmd_vel",10);
+	bumper_information_subscriber = n.subscribe("/bumper_value",1000,stop_robot);
+	laser_scan_sub = n.subscribe("/scan",1000,assign_scan);
 
 	//TODO: Define a drive /ball_chaser/command_robot service with a handle_drive_request callback function
 	ros::ServiceServer vel_commands = n.advertiseService("ball_chaser/command_robot",relay_robot_velocities);
---
+++ catkin_ws/src/my_robot/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/workspace/catkin_ws/src/my_robot/build/devel:$CMAKE_PREFIX_PATH"
+export PATH="/opt/ros/kinetic/bin:/usr/bin:/root/miniconda3/bin:/opt/pytorch/bin:/opt/utils/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/opt/VirtualGL/bin:/opt/TurboVNC/bin:/opt/swift/swift-4.0.3-RELEASE-ubuntu16.04/usr/bin"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/workspace/catkin_ws/src/my_robot/build/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/workspace/catkin_ws/src/my_robot:$ROS_PACKAGE_PATH"
